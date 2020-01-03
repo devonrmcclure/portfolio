f4986664de59ea9f7bbc9d114791bd770e36f0ab
devonrmcclure <devon.r.mcclure@gmail.com>
2020-01-03 16:13:52 -0700

    remake with bootstrap

--- public/index.php
+++ public/index.php
@@ -1,3 +1,84 @@
-<?php declare(strict_types = 1); 
+<!DOCTYPE html>
+<html lang="en">
 
-require __DIR__ . '/../src/Bootstrap.php';
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+	<link rel="stylesheet" href="/assets/css/style.css">
+	<title>Devon McClure</title>
+</head>
+
+<body>
+
+	<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+		<div class="container">
+			<a class="navbar-brand" href="#">Devon McClure</a>
+			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+				<span class="navbar-toggler-icon"></span>
+			</button>
+
+			<div class="collapse navbar-collapse" id="navbarSupportedContent">
+				<ul class="navbar-nav mr-auto">
+					<li class="nav-item">
+						<a class="nav-link" href="https://github.com/devonrmcclure">Github</a>
+					</li>
+
+					<li class="nav-item">
+						<a class="nav-link" href="https://github.com/phinocio">Second Github</a>
+					</li>
+
+				</ul>
+			</div>
+		</div>
+	</nav>
+
+	<main role="main" class="container">
+		<div class="row">
+			<div class="col-md-4">
+				<div class="card" style="width: 18rem;">
+					<div class="card-body">
+						<h5 class="card-title">About</h5>
+						<p class="card-text">Self taught web developer from Calgary, Canada. Started learning programming in 2010 and have continued since then. Learning a variety of tools and languages including JavaScript, PHP, React, Typescript, VueJS, Laravel, etc.</p>
+					</div>
+				</div>
+			</div>
+
+
+			<div class="col-sm-8">
+				<div class="row">
+					<div class="card" style="width: 18rem;">
+						<div class="card-body">
+							<h5 class="card-title">Nazeem <small>Discord Bot</small></h5>
+							<p class="card-text">A simple Discord bot written in Typescript and Discord.js.</p>
+							<a href="https://github.com/phinocio/nazeem" class="btn btn-primary">Github Repo</a>
+						</div>
+					</div>
+
+					<div class="card" style="width: 18rem;">
+						<img src="/assets/img/vgcteamshare.png" class="card-img-top" alt="vgc teamshare">
+						<div class="card-body">
+							<h5 class="card-title">VGC Teamshare</h5>
+							<p class="card-text">A simple teamsharing site for Pokemon VGC. Currently being re-made with Node and React.</p>
+							<a href="https://github.com/phinocio/vgcteamshare" class="btn btn-primary">Github Repo</a>
+						</div>
+					</div>
+
+
+					<div class="card" style="width: 18rem;">
+						<div class="card-body">
+							<h5 class="card-title">Batteries POS</h5>
+							<p class="card-text">A POS system that was in progress for my old retail job. Sadly didn't get completed enough before I left. Split between a front-end repo and an API repo.</p>
+							<a href="https://github.com/devonrmcclure/BatteriesPOS/tree/vuejs-remake" class="btn btn-primary">Front End Repo</a>
+							<a href="https://github.com/devonrmcclure/batteries-api" class="btn btn-primary">API Repo</a>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+
+	</main>
+</body>
+
+</html>
